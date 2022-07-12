0449efe8a978e9c788902d8b56ef252ce481d286
EttoreBarcella <100206174+EttoreBarcella@users.noreply.github.com>
2022-07-12 17:32:35 +0200

    added data tables + datas

--- database/migrations/2022_07_12_134859_create_trains_table.php
+++ database/migrations/2022_07_12_134859_create_trains_table.php
@@ -1,14 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
 {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
-
             $table->string('company', 30);
             $table->string('departure_station', 50);
             $table->string('arrival_station', 50);
             $table->dateTime('departure_time', 0);
             $table->dateTime('arrival_time', 0);
-            $table->date('departure_date');
-            $table->date('arrival_date');
             $table->string('train_code', 5);
             $table->unsignedTinyInteger('carriages');
             $table->boolean('on_time')->default(true);
@@ -16,3 +27,14 @@
             $table->timestamps();
         });
     }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
